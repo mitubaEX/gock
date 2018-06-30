da8cd960f0b27e74fccf1eb6359b4159ba489c8c
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-07-01 01:08:18 +0900

    feat: add function of posting message to slack

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 )
 
+func getEnv() (string, error) {
+	if env := os.Getenv("WEBHOOK_URL"); env != "" {
+		return env, nil
+	} else {
+		return "", errors.New("WEBHOOK_URL not found")
+	}
+}
+
 func main() {
+	webhookURL, err := getEnv()
+	if err != nil {
+		panic(err)
+	}
+	log.Println(webhookURL)
+
 	execCommand := strings.Join(os.Args[1:], " ")
 	log.Println(execCommand)
 	result, err := exec.Command("sh", "-c", execCommand).Output()
@@ -15,4 +32,24 @@ func main() {
 		panic(err)
 	}
 	log.Println(string(result))
+
+	postData := `{"text": "` + "```$ " + execCommand + "\n" + string(result) + "```" + `"}`
+
+	req, err := http.NewRequest(
+		"POST",
+		webhookURL,
+		bytes.NewBuffer([]byte(postData)),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 }
